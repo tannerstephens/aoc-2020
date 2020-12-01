bd239a47348d343d4278845cc2c35986f6cd756d
Tanner Stephens <tannerrstephens@gmail.com>
2020-11-30 23:49:02 -0600

    Use itertools for part 2

--- day1/run.py
+++ day1/run.py
@@ -3,6 +3,8 @@
 from os.path import dirname, realpath
 dir_path = dirname(realpath(__file__))
 
+from itertools import combinations
+
 with open(f'{dir_path}/input') as f:
   puzzle_input = f.read().split('\n')[:-1]
 
@@ -17,13 +19,11 @@ def find_2020_mul(s):
       return look * item
 
 def find_2020_mul_3(s):
-  iterable = list(s)
-  for i, n in enumerate(iterable):
-    for n2 in iterable[i+1:]:
-      look = 2020 - n - n2
+  for n1, n2 in combinations(s, 2):
+    look = 2020 - n1 - n2
 
-      if look in s:
-        return n*n2*look
+    if look in s:
+      return n1*n2*look
 
 def part1():
   pi = parse_input()
