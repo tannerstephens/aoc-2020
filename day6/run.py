5d8e13fcc7e9044f943f1a47b69799a603e70245
Tanner Stephens <tannerrstephens@gmail.com>
2020-12-05 23:27:50 -0600

    Day 6

---
+++ day6/run.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from os.path import dirname, realpath
+dir_path = dirname(realpath(__file__))
+
+with open(f'{dir_path}/input') as f:
+  puzzle_input = f.read().split('\n\n')
+
+def parse_input():
+  return puzzle_input[:]
+
+def get_anyone_count_for_group(group):
+  work = group.split()
+
+  answers = set()
+
+  for line in work:
+    answers.update(list(line))
+
+  return len(answers)
+
+def get_everyone_count_for_group(group):
+  work = group.split()
+
+  answers = set(list(work[0]))
+
+  for line in work[1:]:
+    answers = answers.intersection(list(line))
+
+  return len(answers)
+
+def part1():
+  pi = parse_input()
+
+  return sum(map(get_anyone_count_for_group, pi))
+
+def part2():
+  pi = parse_input()
+
+  return sum(map(get_everyone_count_for_group, pi))
+
+def main():
+  part1_res = part1()
+  print(f'Part 1: {part1_res}')
+
+  part2_res = part2()
+  print(f'Part 2: {part2_res}')
+
+if __name__ == '__main__':
+  main()
